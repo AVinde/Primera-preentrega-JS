eb9fa2c41ed913a65afb13ec01f4b49f8fa4e963
Alvaro Vinderola <alvaro.vinderola@gmail.com>
2023-03-04 15:41:00 -0300

    Subida a github

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+let monto = parseInt(prompt("Ingrese cuanto dinero quiere invertir."));
+let meses = parseInt(prompt("Ingresar la cantidad de meses a invertir."));
 
+if (meses <= 12 && meses > 0){
+    alert("El interés anual es de 40%");
+    for (let i = 0; i <= meses; i++) {
+        let interes12 = i * 0.04 * monto + monto;
+        alert("Su capital en el mes " + i + " es de " + interes12 + '.');
+    }
+} else if(meses <= 24 && meses > 12){
+    alert("El interés anual es de 60%");
+    for (let i = 0; i <= meses; i++) {
+        let interes24 = i * 0.06 * monto + monto;
+        alert("Su capital en el mes " + i + " es de " + interes24 + '.');
+    }
+} else if(meses <= 36 && meses > 24){
+    alert("El interés anual es de 80%");
+    for (let i = 0; i <= meses; i++) {
+        let interes36 = i * 0.08 * monto + monto;
+        alert("Su capital en el mes " + i + " es de " + interes36 + '.');
+    }
+} else if(meses < 1){
+    alert('La cantidad de meses indicada es menor a la mínima permitida.')
+} else if(meses > 36){
+    alert('La cantidad de meses indicada es mayor a la náxima permitida.')
+}
 
-class 
+alert ('Gracias por elegirnos.')
