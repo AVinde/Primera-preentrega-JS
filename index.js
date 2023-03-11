b952909d4a1cb65c87975e7ad884310be2427e2e
Alvaro Vinderola <alvaro.vinderola@gmail.com>
2023-03-10 23:59:10 -0300

    Se agrego una funcion

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 let monto = parseInt(prompt("Ingrese cuanto dinero quiere invertir."));
 let meses = parseInt(prompt("Ingresar la cantidad de meses a invertir."));
+let interesAnual= parseInt (prompt("Ingresa la tasa anual"));
 
-if (meses <= 12 && meses > 0){
-    alert("El interés anual es de 40%");
+function interesMensual() {
+    let division = interesAnual / meses / 100;
+    return division;
+  }
+  let interes = interesMensual();
+  
+  if (meses >= 1 && meses <= 36) {
+    alert("El interés anual es del " + interesAnual + "%.");
     for (let i = 0; i <= meses; i++) {
-        let interes12 = i * 0.04 * monto + monto;
-        alert("Su capital en el mes " + i + " es de " + interes12 + '.');
+      let montoAcumulado = i * interes * monto + monto;
+      alert(
+        "Su capital en el mes número " + i + " es de " + montoAcumulado + "."
+      );
     }
-} else if(meses <= 24 && meses > 12){
-    alert("El interés anual es de 60%");
-    for (let i = 0; i <= meses; i++) {
-        let interes24 = i * 0.06 * monto + monto;
-        alert("Su capital en el mes " + i + " es de " + interes24 + '.');
-    }
-} else if(meses <= 36 && meses > 24){
-    alert("El interés anual es de 80%");
-    for (let i = 0; i <= meses; i++) {
-        let interes36 = i * 0.08 * monto + monto;
-        alert("Su capital en el mes " + i + " es de " + interes36 + '.');
-    }
-} else if(meses < 1){
-    alert('La cantidad de meses indicada es menor a la mínima permitida.')
-} else if(meses > 36){
-    alert('La cantidad de meses indicada es mayor a la náxima permitida.')
-}
+  } else {
+    alert('La cantidad de meses elegida es inválida.')
+  }
 
 alert ('Gracias por elegirnos.')
